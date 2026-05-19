0ecc707c0a30535797cd3f1d35443e617e2fbf2e
kkemboi <kemboikelvin185@gmail.com>
2026-05-18 20:15:28 -0400

    Fixed variable name inconcistencies

--- src/SJF.cpp
+++ src/SJF.cpp
@@ -19,28 +19,29 @@ int main(){
     vector<Process> processes={p1, p2, p3};
 
     int n=processes.size();
-    int current_time=0;
+    int time=0;
     int completed=0;
     
-    vector<bool> done(n, false);
+    vector<bool> done(n, false); //completed processes
     while(completed<n){
-        int shortest_p=-1; //candidate process;
+        int shortest=-1; //candidate process;
         for(int i=0;i<n;i++){
             if(done[i]) continue; //skip completed processes
-            if(processes[i].arrival_time>current_time) continue; //skip unarrived processes
+            if(processes[i].arrival_time>time) continue; //skip unarrived processes
             if(shortest==-1 || processes[i].burst_time<processes[shortest].burst_time) shortest=i; //update candidate process
         }
-        if(shortest_p==-1){
-           int next_arrival=INT_MAX; //start with max integer value
+        //if nothing has arrived yet
+        if(shortest==-1){
+           int next_arrival=INT_MAX; //max integer value
            
+           //find earliest arrival for uncompleted processes
            for(int i=0;i<n;i++){
-                //find earliest arrival for uncompleted processes
                 if(!done[i]){
                     next_arrival=min(next_arrival, processes[i].arrival_time);
                 }
            }
            //jump to that arrival time
-           current_time=arrival_time;
+           time=next_arrival;
            continue;
         }
         
